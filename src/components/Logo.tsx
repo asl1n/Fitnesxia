e12174a340eeaf70452a3be3dedc7019568897ea
asl1n <122009391+asl1n@users.noreply.github.com>
2025-01-21 12:04:52 +0545

    logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,8 +1,8 @@
-import logo from "../assets/img/logo.png";
+import "/logo.png";
 
 export default function Logo(){
     return <div className="flex items-center">
-          <img src={logo} alt="Logo" className="w-16 h-11" />
+          <img src="logo.png" alt="Logo" className="w-16 h-11" />
           <span className="text-xl font-bold text-white">FITNESXIA</span>
         </div>
 }
